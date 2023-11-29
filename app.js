047d9b46f1e1fa593e3c6f87eff1d42697bb3da9
Hussein Kamal <107565196+husseynkamal@users.noreply.github.com>
2023-11-29 12:59:38 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3290,3 +3290,67 @@ console.log(palindromeIndex("bbaa"));
 //=========================================
 console.log("#".repeat(30));
 // [134]
+function activityNotifications(expenditure, d) {
+  const RANGE = 201;
+  let notifications = 0;
+  const getDoubleMedian = (count, d) => {
+    const N = count.length;
+    const countPrefix = Array.from(count);
+    for (let i = 0; i < N; i++) {
+      countPrefix[i] += countPrefix[i - 1] || 0;
+    }
+    let a = 0;
+    let b = 0;
+
+    if (d % 2 === 0) {
+      let first = Math.floor(d / 2);
+      let second = first + 1;
+      let i = 0;
+      for (; i < RANGE; i++) {
+        if (first <= countPrefix[i]) {
+          a = i;
+          break;
+        }
+      }
+      for (; i < RANGE; i++) {
+        if (second <= countPrefix[i]) {
+          b = i;
+          break;
+        }
+      }
+    } else {
+      let first = Math.floor(d / 2) + 1;
+      for (let i = 0; i < RANGE; i++) {
+        if (first <= countPrefix[i]) {
+          a = i;
+          b = i;
+          break;
+        }
+      }
+    }
+
+    return a + b;
+  };
+
+  const count = Array(RANGE).fill(0);
+  for (let i = 0; i < d; i++) {
+    count[expenditure[i]]++;
+  }
+
+  for (let i = d; i < expenditure.length; i++) {
+    const median = getDoubleMedian(count, d);
+    if (expenditure[i] >= median) {
+      notifications++;
+    }
+
+    count[expenditure[i]]++;
+    count[expenditure[i - d]]--;
+  }
+
+  return notifications;
+}
+
+console.log(activityNotifications([10, 20, 30, 40, 50], 3));
+//=========================================
+console.log("#".repeat(30));
+// [135]
