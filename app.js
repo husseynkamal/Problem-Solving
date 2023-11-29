bf0307b373454b7378838e6d326bdbb5abc732fe
Hussein Kamal <107565196+husseynkamal@users.noreply.github.com>
2023-11-29 13:19:47 +0200

    Update app.js

--- app.js
+++ app.js
@@ -3354,3 +3354,26 @@ console.log(activityNotifications([10, 20, 30, 40, 50], 3));
 //=========================================
 console.log("#".repeat(30));
 // [135]
+function anagram(s) {
+  let midpoint = Math.floor(s.length / 2);
+  let firstHalf = s.substring(0, midpoint);
+  let secondHalf = s.substring(midpoint);
+  let count = 0;
+
+  if (s.length % 2 !== 0) {
+    return -1;
+  }
+
+  for (let i = 0; i < firstHalf.length; i++) {
+    secondHalf.includes(firstHalf[i])
+      ? (secondHalf = secondHalf.replace(firstHalf[i], ""))
+      : count++;
+  }
+
+  return count;
+}
+
+console.log(anagram("xaxbbbxx"));
+//=========================================
+console.log("#".repeat(30));
+// [136]
